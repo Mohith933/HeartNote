0f7874e9744291137e3bd73c8d4395283a75c806
Bhimavarpu Mohith Sai <bmstpt1@gmail.com>
2026-04-22 14:59:40 +0530

    Update urls.py

--- write/urls.py
+++ write/urls.py
@@ -4,9 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("",views.home,name="home"),
-    path("aiwrite/", views.aiwrite, name="aiwrite"),
-    path("dashboard/",views.dashboard,name="dashboard"),
+    path("",views.home,name="home"),   path("dashboard/",views.dashboard,name="dashboard"),
     path("api/generate/", views.generate_text, name="generate_text"),
     path("api/dashboard/", views.generate_dashboard, name="generate_dashboard"),
     path("admin_stats/",views.admin_stats, name="admin_stats"),
